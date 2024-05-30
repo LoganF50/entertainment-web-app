5ed560c100e279cd2585adccf0027c73d052f067
Logan Faschingbauer <faschingbauerl@yahoo.com>
2024-05-30 09:58:17 -0500

    configure routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,41 @@
+import PageLayout from "@layouts/PageLayout";
+import { ShowProvider } from "@providers/showProvider";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <PageLayout />,
+    children: [
+      {
+        index: true,
+        element: <div>home</div>,
+      },
+      {
+        path: "movies",
+        element: <div>movies</div>,
+      },
+      {
+        path: "tv",
+        element: <div>tv</div>,
+      },
+      {
+        path: "bookmarked",
+        element: <div>bookmarked</div>,
+      },
+      {
+        path: "*",
+        element: <div>not found</div>,
+      },
+    ],
+  },
+]);
+
 function App() {
   return (
-    <>
-      <div>app</div>
-    </>
+    <ShowProvider>
+      <RouterProvider router={router} />
+    </ShowProvider>
   );
 }
 
