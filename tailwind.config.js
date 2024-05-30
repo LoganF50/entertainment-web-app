c2a627c9524e54a367b773924715dcc9d26f404f
Logan Faschingbauer <faschingbauerl@yahoo.com>
2024-05-30 09:50:16 -0500

    add dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        outfit: ["Outfit", "sans-serif"],
+      },
+      colors: {
+        "app-red": "#FC4747",
+        "app-dark-blue": "#10141E",
+        "app-light-blue": "#5A698F",
+        "app-blue": "#161D2F",
+        "app-white": "#FFFFFF",
+      },
+    },
   },
   plugins: [],
 };
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,27 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+
+const aliases = [
+  ["components", "components"],
+  ["context", "context"],
+  ["data", "data"],
+  ["hooks", "hooks"],
+  ["icons", "assets/icons"],
+  ["interfaces", "interfaces"],
+  ["layouts", "layouts"],
+  ["pages", "pages"],
+  ["providers", "providers"],
+  ["utils", "utils"],
+];
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: aliases.map((alias) => ({
+      find: `@${alias[0]}`,
+      replacement: path.resolve(__dirname, `src/${alias[1]}`),
+    })),
+  },
+});
